48216231ff5492949dc577b568656122cbc2eb5f
Bruno David Martins <dbrunobruno369@gmail.com>
2026-02-27 22:58:11 -0300

    teste

---
+++ listas e tuplas.py
@@ -0,0 +1,5 @@
+telefones = { "bruno": 123456789, "maria": 987654321, "joao": 555555555 }
+
+for k, i in telefones.items():
+    if k == "bruno":
+        print(f"O numero de telefone do Bruno é: {i} ")
