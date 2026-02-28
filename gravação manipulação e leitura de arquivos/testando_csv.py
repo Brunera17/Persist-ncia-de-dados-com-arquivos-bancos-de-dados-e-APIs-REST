661508ef429340ca301dc88ac30a4e467266a914
Bruno David Martins <dbrunobruno369@gmail.com>
2026-02-27 23:56:13 -0300

    csv

--- gravação manipulação e leitura de arquivos/testando_csv.py
+++ gravação manipulação e leitura de arquivos/testando_csv.py
@@ -4,3 +4,8 @@ with open('dados.csv', 'w') as f:
     escritor = csv.writer(f)
     escritor.writerow(['nome', 'idade'])
     escritor.writerow(['Bruno', '24'])
+
+with open('dados.csv', newline='') as f:
+    leitor = csv.reader(f)
+    for linha in leitor:
+        print(linha)
