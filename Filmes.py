2a0a47155dbd514cec75be63be5fbf14eb2ae85b
Bruno David Martins <dbrunobruno369@gmail.com>
2026-02-27 23:26:42 -0300

    atualizando

--- Filmes.py
+++ Filmes.py
@@ -1,8 +1,31 @@
-filmes = { "Ação": ["Piratas do caribe", "Duro de matar", "Homem aranha"],"Comédia": ["Um maluco no Golf", "Gente grande", "Derepente 30!"]}
+filmes = {
+    "Ação": {
+        "Piratas do caribe": "14/12/2025",
+        "Duro de matar": "30/01/2026",
+        "Homem aranha": "15/02/2026"
+    },
+    "Comédia": {
+        "Um maluco no Golf": None,
+        "Gente grande": None,
+        "Derepente 30!": None
+    }
+}
+for categoria, lista in filmes.items():
+    print(f"\nMeus filmes de {categoria} favoritos:\n")
+    for filme, data in lista.items():
+        if data:
+            print(f"{filme} - {data}")
+        else:
+            print(f"{filme}")
 
-for k, i in filmes.items():
-    print(f"\nMeus filmes de {k} favoritos:\n" + ";\n".join(i) + ".\n")
+filmes["Comédia"]["Um maluco no Golf"] = "20/02/2026"
 
-datas = "14/12/2025", "30/01/2026", "15/02/2026"
+for categoria, lista in filmes.items():
+    print(f"\nMeus filmes de {categoria} favoritos:\n")
+    for filme, data in lista.items():
+        if data:
+            print(f"{filme} - {data}")
+        else:
+            print(f"{filme}")
 
-filmes["Ação"["Piratas do caribe"[0]=datas[0]]]
+
